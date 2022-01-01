9dccf752f3031db2bd3d12db6a0ae124f88badac
bulletmagnet <bulletmagnet.123@gmail.com>
2021-12-31 18:38:49 -0800

    paint works now i have a paint hide button thanks bitch

--- src/WoodCuttingScript.java
+++ src/WoodCuttingScript.java
@@ -22,9 +22,14 @@ import org.dreambot.api.wrappers.interactive.Model;
 import org.dreambot.api.wrappers.widgets.message.Message;
 import picocli.CommandLine;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.IOException;
 import java.lang.reflect.Array;
+import java.net.URL;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
@@ -32,7 +37,7 @@ import java.util.concurrent.TimeUnit;
 import static org.dreambot.api.methods.interactive.GameObjects.all;
 
 @ScriptManifest(category = Category.WOODCUTTING, name = "Bullets Woodcutter", author = "Bulletmagnet", version = 1.0)
-public class WoodCuttingScript extends AbstractScript implements ChatListener {
+public class WoodCuttingScript extends AbstractScript implements ChatListener, MouseListener {
 
     Area YewTrees = new Area(2717, 3467, 2704, 3457);
     Area MagicTrees = new Area(2707, 3395, 2698, 3399);
@@ -44,6 +49,7 @@ public class WoodCuttingScript extends AbstractScript implements ChatListener {
     GameObject treeStump = null;
     private Timer timeRan;
     int beginningXP;
+    int beginningLevel;
     int currentXp;
     int xpGained;
     private final BasicStroke stroke1 = new BasicStroke(5);
@@ -51,6 +57,21 @@ public class WoodCuttingScript extends AbstractScript implements ChatListener {
     public int TreesChopped = 0;
     public ZenAntiBan antiban;
     public String LevelOrMoneyOrWillowLevel;
+    private final Image showImage = getImage("https://pbs.twimg.com/profile_images/689269969519939584/TYr_hQdq_400x400.jpg");
+    private final Image hideImage = getImage("https://pbs.twimg.com/profile_images/689269969519939584/TYr_hQdq_400x400.jpg");
+    private final Image porn = getImage("https://static1.e621.net/data/bb/d6/bbd6c18b06f6ac26b6292bde83db3f3d.png");
+    boolean hidePaint = false;
+
+
+    private Image getImage(String url)
+    {
+        try
+        {
+            return ImageIO.read(new URL(url));
+        }
+        catch (IOException e) {}
+        return null;
+    }
 
     public String BankOrDrop;
 
@@ -67,11 +88,13 @@ public class WoodCuttingScript extends AbstractScript implements ChatListener {
 
 
     public void onStart() {
+
         antiban = new ZenAntiBan(this);
         drawMouseUtil.setRandomColor();
         timeRan = new Timer();
         SkillTracker.start();
         beginningXP = Skills.getExperience(Skill.WOODCUTTING);
+        beginningLevel = Skills.getRealLevel(Skill.WOODCUTTING);
         GUI();
         if (StartScript) {
             if (hasEquipment() && Inventory.onlyContains(axe)) {
@@ -272,39 +295,80 @@ public class WoodCuttingScript extends AbstractScript implements ChatListener {
     public int cut;
 
 
+
+
+
     public void onPaint(Graphics2D g) {
-        long ttl = SkillTracker.getTimeToLevel(Skill.WOODCUTTING);
-        long timeTNL = ttl;
-        g.setColor(new Color(51, 51, 51, 140));
-        g.drawRect(10, 250, 300, 400);
-        g.fillRect(10, 250, 300, 400);
-        g.setStroke(stroke1);
-        Timer.formatTime(ttl);
-        Polygon tile = Map.getPolygon(getLocalPlayer().getTile());
-        g.drawPolygon(tile);
-        currentXp = Skills.getExperience(Skill.WOODCUTTING);
-        xpGained = currentXp - beginningXP;
-        SkillTracker.getTimeToLevel(Skill.WOODCUTTING);
-        g.setColor(Color.CYAN);
-        g.drawString("Bullets Chopper", 140, 280);
-        g.drawString("Anti-Ban Status: " + (antiban.getStatus().equals("") ? "Inactive" : antiban.getStatus()), 150, 320);
-        g.drawString("current xp: " + currentXp, 150, 450);
-        g.drawString("Trees Chopped: " + cut, 150, 425);
-        g.drawString("Time Ran: " + timeRan.formatTime(), 150, 400);
-        g.drawString("XP GAINED: " + xpGained, 150, 375);
-        g.drawString("Current level: " + Skills.getRealLevel(Skill.WOODCUTTING), 150, 350);
-        g.drawString("Time tell level: " + ft(ttl), 20, 300);
-        g.drawString("bank or drop:  " + BankOrDrop, 15, 320);
-        g.drawString("Level or Money: " + LevelOrMoneyOrWillowLevel, 15, 340);
-        drawMouseUtil.drawRandomMouse(g);
-        drawMouseUtil.drawRandomMouseTrail(g);
-        int i = 0;
-        for (i = 0; i < 10; i++ ){
-            List<GameObject> Tree = all("Willow");
-            Tree.get(i).getModel().drawWireFrame(g);
+        if (!hidePaint) {
+            g.drawRect(openPaint.x, openPaint.y, openPaint.width, openPaint.height);
+            g.drawImage(this.hideImage, rect.x, rect.y, rect.width, rect.height, null);
+            long ttl = 0;
+            Timer.formatTime(ttl);
+            Polygon tile = Map.getPolygon(getLocalPlayer().getTile());
+            g.drawPolygon(tile);
+            currentXp = Skills.getExperience(Skill.WOODCUTTING);
+            xpGained = currentXp - beginningXP;
+            SkillTracker.getTimeToLevel(Skill.WOODCUTTING);
+            ttl = SkillTracker.getTimeToLevel(Skill.WOODCUTTING);
+            long timeTNL = ttl;
+            g.setColor(new Color(51, 51, 51, 140));
+            g.drawRect(10, 250, 300, 400);
+            g.fillRect(10, 250, 300, 400);
+            g.setStroke(stroke1);
+            g.drawPolygon(tile);
+            g.setColor(Color.CYAN);
+            g.drawString("Bullets Chopper", 140, 280);
+            g.drawString("Time tell level: " + ft(ttl), 15, 300);
+            g.drawString("Anti-Ban Status: " + (antiban.getStatus().equals("") ? "Inactive" : antiban.getStatus()), 150, 320);
+            g.drawString("bank or drop:  " + BankOrDrop, 15, 320);
+            g.drawString("Current level: " + Skills.getRealLevel(Skill.WOODCUTTING), 150, 355);
+            g.drawString("Starting Level: " + beginningLevel, 150, 365);
+            g.drawString("XP GAINED: " + xpGained, 150, 375);
+            g.drawString("Time Ran: " + timeRan.formatTime(), 150, 385);
+            g.drawString("Trees Chopped: " + cut, 150, 420);
+            g.drawString("current xp: " + currentXp, 150, 430);
+
+
+            g.drawString("Level or Money: " + LevelOrMoneyOrWillowLevel, 15, 340);
+            drawMouseUtil.drawRandomMouse(g);
+            drawMouseUtil.drawRandomMouseTrail(g);
+            int i = 0;
+            for (i = 0; i < 10; i++) {
+                g.setColor(Color.RED);
+                List<GameObject> Tree = all("Willow");
+                Tree.get(i).getModel().drawWireFrame(g);
+            }
+        } else {
+            g.drawImage(this.porn, 200, 200, 400, 400, null);
+            g.drawRect(closePaint.x, closePaint.y, closePaint.width, closePaint.height);
+            g.drawImage(this.hideImage, rect.x, rect.y, rect.width, rect.height, null);
+            g.dispose();
         }
 
+
+
+
+
     }
+    Rectangle rect = new Rectangle(296, 444, 200, 100);
+    Rectangle closePaint = rect;
+    Rectangle openPaint = rect;
+    Point p;
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+        p = e.getPoint();
+
+        if (closePaint.contains(p) && !hidePaint) {
+
+            hidePaint = true;
+
+        } else if (openPaint.contains(p) && hidePaint) {
+
+            hidePaint = false;
+        }
+    }
+
 
     private String ft(long duration) {
         String res = "";
@@ -325,7 +389,25 @@ public class WoodCuttingScript extends AbstractScript implements ChatListener {
         return res;
     }
 
+    @Override
+    public void mousePressed(MouseEvent e) {
 
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
 
 
